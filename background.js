8da1da9f8c28a9adfca8cc2aa506a96248ba90eb
thebrucecgit <brucecccccc@gmail.com>
2019-08-02 16:55:46 +1200

    No OnInstall

--- background.js
+++
@@ -1,6 +0,0 @@
-const onInstalledURL = "https://clock.thebrucecweb.com/oninstall.html"
-chrome.runtime.onInstalled.addListener((object)=>{
-    chrome.tabs.create({url: onInstalledURL}, (tab)=>{
-        console.log(`New tab launched with ${onInstalledURL}`);
-    });
-});
