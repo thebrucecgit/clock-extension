e17d07c4e6b93de94ddc8046d3debe408b642ba5
thebrucecgit <brucecccccc@gmail.com>
2019-08-18 11:54:17 +1200

    Height sizing and larger icon

--- content.js
+++ content.js
@@ -1,5 +1,18 @@
+function resizeIFrameOnLoad(e) {
+    console.log(e)
+    console.log(e.target.contentWindow.document.body.scrollHeight)
+}
+
 const sec = document.querySelector(".small-12.large-4.columns");
-const iframe = document.createElement("iframe");
-iframe.src="https://clock.thebrucecweb.com";
-// iframe.setAttribute("sandbox", "allow-scripts");
-if(sec && window.location.pathname === "/") sec.insertBefore(iframe, sec.childNodes[0]);
+if (window.location.pathname === "/" && sec) {
+    const h2 = document.createElement("h2");
+    h2.classList.add("subheader");
+    h2.appendChild(document.createTextNode("Clock"));
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://clock.thebrucecweb.com";
+    iframe.height = "200";
+    iframe.onload = resizeIFrameOnLoad;
+    // iframe.setAttribute("sandbox", "allow-scripts");
+    sec.insertBefore(iframe, sec.childNodes[0]);
+    sec.insertBefore(h2, iframe);
+}
