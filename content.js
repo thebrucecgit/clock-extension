f9b0fdc265a6de5e7c0343cbc149e787946ccbea
thebrucecgit <brucecccccc@gmail.com>
2019-09-27 10:16:28 +1200

    Removed useless code and added comments

--- content.js
+++ content.js
@@ -1,18 +1,17 @@
-function resizeIFrameOnLoad(e) {
-    console.log(e)
-    console.log(e.target.contentWindow.document.body.scrollHeight)
-}
-
 const sec = document.querySelector(".small-12.large-4.columns");
+// Only displays clock on homepage of School Portal
 if (window.location.pathname === "/" && sec) {
+    // "Clock" Header
     const h2 = document.createElement("h2");
     h2.classList.add("subheader");
     h2.appendChild(document.createTextNode("Clock"));
+
+    // IFRAME element linking to clock webpage
     const iframe = document.createElement("iframe");
     iframe.src = "https://clock.thebrucecweb.com";
-    iframe.height = "200";
-    iframe.onload = resizeIFrameOnLoad;
-    // iframe.setAttribute("sandbox", "allow-scripts");
+    iframe.height = "250";
+
+    // Insertion
     sec.insertBefore(iframe, sec.childNodes[0]);
     sec.insertBefore(h2, iframe);
 }
